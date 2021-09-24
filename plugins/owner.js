9cb3369e5ac4f1cb43936853785d96019303ca17
Brutal-sir <89448528+Brutal-sir@users.noreply.github.com>
2021-09-24 17:35:26 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -30,13 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'ㅤㅤ *════ᴀͥɴͭᴜᷤܓᴍᴡͫᴏͤʟ════*
-
-*╔══════════════════╗*
- *owner:wa.me/917356484508*
-*╚══════════════════╝*
-
-         ㅤ *Creator: Ᏼ𝚁𝚄𝚃ꫝ𝙻 🖤*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'AnuMwol by BrutalSir' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
