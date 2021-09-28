e5f86e3df6cb21455df46678e8bb6d5c22733e2e
Brutal-sir <89448528+Brutal-sir@users.noreply.github.com>
2021-09-28 17:24:13 +0530

    👀

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
     OWNER: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
     ALL: process.env.ALL_CAPTION === undefined ? 'Made By AnuMwol' : process.env.ALL_CAPTION,
-    MENTION: process.env.TAG_REPLY === undefined ? '917356484508@s.whatsapp.net' : process.env.TAG_REPLY,
+    MENTION: process.env.TAG_REPLY === undefined ? '918921483992@s.whatsapp.net' : process.env.TAG_REPLY,
     ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
     PLKS: process.env.THERI_LIST === undefined ? false : process.env.THERI_LIST,
     MUTEMSG: process.env.MUTE_MESSAGE === undefined ? 'default' : process.env.MUTE_MESSAGE,
