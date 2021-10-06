3247b00360220267fddfda2e35e2a242dca8a089
br7.tl_x <89448528+Brutal-sir@users.noreply.github.com>
2021-10-06 09:34:32 +0530

    Update Anu.js

--- plugins/Anu.js
+++ plugins/Anu.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
-const fs = require('fs');
+const fs = require('../fs');
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
