01e1a94aece615570c30800230f10b74df4228ec
br7.tl_x <89448528+Brutal-sir@users.noreply.github.com>
2021-10-11 10:06:59 +0530

    Update julie.js

--- julie.js
+++ julie.js
@@ -56,4 +56,18 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     await conn.connect();
 }
 
-whatsAsena()
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// PLK Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ PHONE: process.env.NUMBER === undefined ? '+917356484508' : process.env.NUMBER,   
+
+
+};
