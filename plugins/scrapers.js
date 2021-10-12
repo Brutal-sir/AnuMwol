8889e413c0eba4fa7dc411a5fe7ae72c5753e634
br7.tl_x <89448528+Brutal-sir@users.noreply.github.com>
2021-10-12 07:19:01 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1082,23 +1082,41 @@ else if (config.WORKTYPE == 'public') {
         });
     }));
     
-    Julie.addCommand({pattern: 'owner', fromMe: false, desc: Lang.NUMBER}, (async (message, match) => {
+    Julie.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-            const p_lk = 'BEGIN:VCARD\n'
+    const vcard = 'BEGIN:VCARD\n' // saidali said
 
             + 'VERSION:3.0\n' 
 
-            + 'FN:' + Config.OWN_NAME + '\n' //created afnanplk, please copy this with credit..
+            + 'FN:Brutal🖤\n' //coded by saidali   not copy this
 
-            + 'ORG:Brutal🖤;\n' 
+            + 'ORG:script SAIDALI;\n' // the organization of the contact
 
-            + 'TEL;type=CELL;type=VOICE;waid=' + Config.PHONE + ':' + Config.PHONE + ' \n'
+            + 'TEL;type=CELL;type=VOICE;waid=917356484508:918129624395\n' //created saidali
 
             + 'END:VCARD'
 
-await message.client.sendMessage(message.jid, {displayname: "Brutal🖤", vcard: p_lk}, MessageType.contact);
+    await message.client.sendMessage(message.jid,{displayname: "Brutal🖤", vcard: vcard}, MessageType.contact)
 
-  }));
+}))
+
+    Julie.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+
+   const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+
+            + 'VERSION:3.0\n' 
+
+            + 'FN:Config.OWN_NAME\n' // full name
+
+            + 'ORG:script Brutal;\n' // the organization of the contact
+
+            + 'TEL;type=CELL;type=VOICE;waid= Config.NUMBER \n' // WhatsApp ID + phone number
+
+            + 'END:VCARD'
+
+await message.client.sendMessage(message.jid,{displayname: Config.OWN_NAME , vcard: vcard}, MessageType.contact)
+
+}))
 
     Julie.addCommand({ pattern: 'github ?(.*)', fromMe: false, desc: Glang.GİTHUB_DESC }, async (message, match) => {
 
